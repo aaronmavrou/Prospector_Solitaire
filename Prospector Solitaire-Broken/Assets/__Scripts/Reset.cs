b14f1aaeb3833c4625e781b98b22b8e0c6a099de
aaronmavrou <35606894+aaronmavrou@users.noreply.github.com>
2018-03-21 23:20:29 -0400

    Commit

---
+++ Prospector Solitaire-Broken/Assets/__Scripts/Reset.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Reset : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	public void restartLevel() {
+		// Reload the scene, resetting the game
+		Prospector.S.Invoke("ReloadLevel", 0.25f);
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
---
+++ Prospector Solitaire-Broken/Assets/__Scripts/Share.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Share : MonoBehaviour {
+	/*Twitter Variables*/
+
+	//Twitter Share Link
+	string TWITTER_ADDRESS = "http://twitter.com/intent/tweet";
+
+	//Language
+	string TWEET_LANGUAGE ="en";
+
+	//This is the text which you want to show
+	string textToDisplay = "Hey Guys! Check out my score: ";
+
+	/* End of Twitter Variables*/
+
+	// Use this for initialization
+	public void shareScoreOnTwitter () {
+		Application.OpenURL(TWITTER_ADDRESS+"?text=" + WWW.EscapeURL(textToDisplay) + Scoreboard.S.score + "&amp;lang=" + WWW.EscapeURL(TWEET_LANGUAGE));
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
